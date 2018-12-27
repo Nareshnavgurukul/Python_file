0ece8debf9ff9880d7a3a281a7b2a763bb19a642
Nareshnavgurukul <39561340+Nareshnavgurukul@users.noreply.github.com>
2018-12-27 09:58:53 +0530

    Add files via upload

---
+++ assending_order_with_input.py
@@ -0,0 +1,14 @@
+n=[34,4,5,6,7,1,32,43,4434,76,655476235,985,444,-9999,55*67]
+k=[]
+user=input("enter the wanted number ")
+mx=n[0]
+for f in range(user):
+	for i in n:
+		if mx>i:
+			mx=i
+	k.append(mx)
+	n.remove(mx)
+	mx=n[0]
+print k
+
+
