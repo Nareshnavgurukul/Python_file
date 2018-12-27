576c99d8858200a1e3d51d2d7686f8443a12799a
Nareshnavgurukul <39561340+Nareshnavgurukul@users.noreply.github.com>
2018-12-27 10:14:30 +0530

    Update assending_order_with_input.py

--- assending_order_with_input.py
+++ assending_order_with_input.py
@@ -1,14 +1,17 @@
 n=[34,4,5,6,7,1,32,43,4434,76,655476235,985,444,-9999,55*67]
 k=[]
 user=input("enter the wanted number ")
-mx=n[0]
-for f in range(user):
-	for i in n:
-		if mx>i:
-			mx=i
-	k.append(mx)
-	n.remove(mx)
-	mx=n[0]
-print k
+if use <=(len(n)):
+	min=n[0]
+	for f in range(user):
+		for i in n:
+			if min>i:
+				min=i
+		k.append(min)
+		n.remove(min)
+		min=n[0]
+	print k
+else:
+	print "sorry str(user) element are not in this list"
 
 
