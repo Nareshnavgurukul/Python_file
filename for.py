72bcc0e356b3c4240e76a6a50ca2d46869c9bc4c
Nareshnavgurukul <39561340+Nareshnavgurukul@users.noreply.github.com>
2019-01-22 15:07:47 +0530

    Update for.py

--- for.py
+++ for.py
@@ -1,47 +1,47 @@
-# def menu(day):
-#     if day == "monday":
-#         food = "Butter Chicken"
-#     elif day == "tuesday":
-#         food = "Mutton Chaap"
-#     else:
-#         food = "Chole Bhature"
-#     print "Kya main print ho payungi? :-("
-#     return food
-#     print "Lekin main toh pakka nahi print hounga :'("
-# food=menu("monday")
-# print food
+def menu(day):
+    if day == "monday":
+        food = "Butter Chicken"
+    elif day == "tuesday":
+        food = "Mutton Chaap"
+    else:
+        food = "Chole Bhature"
+    print "Kya main print ho payungi? :-("
+    return food
+    print "Lekin main toh pakka nahi print hounga :'("
+food=menu("monday")
+print food
 
 
 
 
 
 
-# n=input()
-# for i in range(1,n+1):
-# 	for j in range(i):
-# 		print i,
-# 	print ""
+n=input()
+for i in range(1,n+1):
+	for j in range(i):
+		print i,
+	print ""
 
 
 
 
-# n=input()
-# i=0
-# while(i<=n):	
-# 	j=0
-# 	while(j<=i):
-# 		print(i),
-# 		j+=1
-# 	print""
-# 	i+=1
+n=input()
+i=0
+while(i<=n):	
+	j=0
+	while(j<=i):
+		print(i),
+		j+=1
+	print""
+	i+=1
 
 
 
 n=input(),
-# i=1
-# while(i<=n):	
-# 	print (str(i)+" ")*i
-# 	i+=1
+i=1
+while(i<=n):	
+	print (str(i)+" ")*i
+	i+=1
 
 
 s=0
