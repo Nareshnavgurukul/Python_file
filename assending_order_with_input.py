ecadb5183b115b87b07b333a11206631c0ee8a0e
Nareshnavgurukul <39561340+Nareshnavgurukul@users.noreply.github.com>
2019-03-26 12:31:11 +0530

    Update assending_order_with_input.py

--- assending_order_with_input.py
+++ assending_order_with_input.py
@@ -1,7 +1,11 @@
-n=[34,4,5,6,7,1,32,43,4434,76,655476235,985,444,-9999,55*67]
+
+N=[34,34,4,5,6,7,1,32,43,4434,76,655476235,985,444,-9999,55*67]
+n = set(N)
+n = list(n)
+list(n)
 k=[]
-user=input("enter the wanted number ")
-if use <=(len(n)):
+user=int(input("enter the wanted number "))
+if user <=(len(n)):
 	min=n[0]
 	for f in range(user):
 		for i in n:
@@ -10,8 +14,6 @@ if use <=(len(n)):
 		k.append(min)
 		n.remove(min)
 		min=n[0]
-	print k
+	print(k)
 else:
-	print "sorry str(user) element are not in this list"
-
-
+	print("sorry str(user) element are not in this list")
